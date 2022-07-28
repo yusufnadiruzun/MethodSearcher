2cb02d795e8ff9cbdc0a08fb4eb90a24b92ef09f
Yusuf Nadir Uzun <69757325+yusufnadiruzun@users.noreply.github.com>
2022-07-28 16:12:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace deneme
 {
     public class Program
     {
-        public static bool isInclude = false;
 
         public static MySqlConnection Mysql = new MySqlConnection("Server=localhost;Database=methods;Uid=root;Pwd='1234';");
         public static MySqlCommand cmd = new MySqlCommand();
@@ -55,50 +54,28 @@ namespace deneme
                 }
             }
         }
-        /*
-        public static void getProperties()
-        {
-            var item = SampleAssembly.GetTypes().Where(x => !(x.FullName.StartsWith("System.")) && !(x.FullName.StartsWith("Microsoft."))).ToList();
-            string ownClassName = item[item.Count - 1].FullName.Substring(0, item[item.Count - 1].FullName.IndexOf("<") - 1);
-            for (int i = item.Count - 2; i > 0; i--) {
-           
-                if (item[i].Namespace == null || item[i].FullName == ownClassName)
-                {
-                        foreach (MethodInfo classitem in classItemList)
-                        {
-                            classitem.ClassName = item[i].Name;
-                            classitem.NameSpace = item[i].Namespace;
-                        }
-                    }
-            }
-        }
-        */
         public static void getParameters()
         {
-            var item = SampleAssembly.GetTypes().Where(x => !(x.FullName.StartsWith("System.")) && !(x.FullName.StartsWith("Microsoft."))).ToList();
-            string ownClassName = item[item.Count - 1].FullName.Substring(0, item[item.Count - 1].FullName.IndexOf("<") - 1);
+            var types = SampleAssembly.GetTypes().Where(x => !(x.FullName.StartsWith("System.")) && !(x.FullName.StartsWith("Microsoft."))).ToList();
+            string ownClassName = types[types.Count - 1].FullName.Substring(0, types[types.Count - 1].FullName.IndexOf("<") - 1);
             string parameters = "";
-            for (int i = item.Count - 2; i > 0; i--)
+
+            for (int i = types.Count - 2; i > 0; i--)
             {
-                if (item[i].Namespace == null || item[i].FullName == ownClassName)
+                if (types[i].Namespace == null || types[i].FullName == ownClassName)
                 {
-                    foreach (var method in item[i].GetMethods())
+                    foreach (var method in types[i].GetMethods())
                     {
                         foreach (var parameteres in method.GetParameters())
                         {
-                            parameters += "," + parameteres.ParameterType.ToString().Substring(parameteres.ParameterType.ToString().IndexOf(".") + 1).ToLower();
-                            
+                            parameters += "," + parameteres.ParameterType.ToString().Substring(parameteres.ParameterType.ToString().IndexOf(".") + 1).ToLower();                           
                         }
                         try
                         {
                             parameters = parameters.Substring(parameters.IndexOf(",")+1);
                             char.ToUpper(parameters[0]);
-
                         }
-                        catch (Exception e)
-                        {
-
-                        }
+                        catch (Exception e){}
                         
                         foreach (MethodInfo classitem in MethodInfoList)
                         {
@@ -107,7 +84,6 @@ namespace deneme
                                 classitem.Parameters = parameters;
                             }
                         }
-                        
                         parameters = "";
                     }
                 }
@@ -117,30 +93,36 @@ namespace deneme
         public static void getMethodBody()
         {
             var assembly = AssemblyDefinition.ReadAssembly(path);
-
+           
             foreach (MethodInfo citem in MethodInfoList)
             {
                 var toInspect = assembly.MainModule
                 .GetTypes()
                 .SelectMany(t => t.Methods
                 .Where(m => m.HasBody)
-                    .Select(m => new { t, m }));
+                .Select(m => new { t, m }));
                 toInspect = toInspect.Where(x => x.t.Name.EndsWith(citem.ClassName) && x.m.Name == citem.MethodName);
 
                 foreach (var method in toInspect)
                 {
-                   
                     foreach (var instruction in method.m.Body.Instructions)
                     {
                         takeInstructions(instruction, citem.Id);
                     }
-                    // Console.WriteLine($"{instruction.OpCode} \"{instruction.Operand}\"");
                 }
             }
         }
         public static void takeInstructions(Instruction instruction, int citem)
         {
-            //string[] userParameter = {"String","String","String","String","String"};
+            string[] instructionArray;
+            string methodInfo;
+            string className = "";
+            string methodNamespace;
+            string returnType;
+            string parameter = "";
+            string[] methodTitle;
+            string[] parameters;
+
             try
             {
                 if (instruction.Operand != null && !instruction.Operand.ToString().Contains("System.Func") &&
@@ -149,23 +131,18 @@ namespace deneme
                     && !instruction.Operand.ToString().Contains("Enumerator") && !instruction.Operand.ToString().Contains("ctor") && !instruction.Operand.ToString().Contains("get_Operand") && !instruction.Operand.ToString().Contains("StartsWith") && !instruction.Operand.ToString().Contains("ToUpper") &&
                     !instruction.Operand.ToString().Contains("ToLower") && !instruction.Operand.ToString().Contains("Equals")
                     && !instruction.Operand.ToString().Contains("Add") && !instruction.Operand.ToString().Contains("ToString") && !instruction.Operand.ToString().Contains("ToList") && !instruction.Operand.ToString().Contains("Write") && !instruction.Operand.ToString().Contains("Dispose") && !instruction.Operand.ToString().Contains("Enumerable") && 
-                    !instruction.Operand.ToString().Contains("IL") && !instruction.Operand.ToString().Contains("Concat"))
-                {
-                    //{ System.Void Searcher.SearcherMethod.MethodSearcher::GetAllMethod(System.String)}
-                    
-                    string[] instructionArray;
+                    !instruction.Operand.ToString().Contains("IL") && !instruction.Operand.ToString().Contains("Concat")){
+
+
                     instructionArray = (instruction.Operand).ToString().Split(":");
-                    string method = instructionArray[2].Substring(instructionArray[1].IndexOf(":") + 1);
-                  
-                    string className = "";
-                    string methodNamespace = instructionArray[0].Substring(instructionArray[0].IndexOf(" ") +1);
-                    string[] namee = methodNamespace.Split(".");
-                    if(namee.Length == 3) { methodNamespace = namee[0] + "." + namee[1]; className = namee[2]; } else { methodNamespace = namee[0]; className = namee[1]; }
-                    
-                   // 
-                    string methodType = instructionArray[0].Substring(0, instructionArray[0].IndexOf(" ") + 1);
-                    string parameter = "";
-                    string[] parameters = method.Split(".");
+                    methodInfo = instructionArray[2].Substring(instructionArray[1].IndexOf(":") + 1);
+                    methodNamespace = instructionArray[0].Substring(instructionArray[0].IndexOf(" ") +1);
+                    methodTitle = methodNamespace.Split(".");
+                   
+                    if(methodTitle.Length == 3) { methodNamespace = methodTitle[0] + "." + methodTitle[1]; className = methodTitle[2]; } else { methodNamespace = methodTitle[0]; className = methodTitle[1]; }
+                   
+                    returnType = instructionArray[0].Substring(0, instructionArray[0].IndexOf(" ") + 1);
+                    parameters = methodInfo.Split(".");
 
                     for (int i = 1; i < parameters.Length; i++)
                     {
@@ -183,8 +160,8 @@ namespace deneme
                         NameSpace = methodNamespace,
                         Classname = className,
                         MainMethodID = citem,
-                        MethodName = method.Substring(0, method.IndexOf("(")),
-                        MethodType = methodType,
+                        MethodName = methodInfo.Substring(0, methodInfo.IndexOf("(")),
+                        MethodType = returnType,
                         Parameters = parameter
 
                     };
@@ -205,8 +182,10 @@ namespace deneme
                 {
                     Console.WriteLine("SQL Bağlantısı Başarılı Bir Şekilde Gerçekleşti");
                     cmd.Connection = Mysql;
-                    cmd.CommandText = "DELETE FROM methods.method";
+                    cmd.CommandText = "DELETE FROM methods.methodinfo";
                     //cmd.CommandText = "";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = "DELETE FROM methods.methodbody";
                     cmd.ExecuteNonQuery();
                 }
                 else { Console.WriteLine("Bağlantı Yapılamadı...!"); }
@@ -222,11 +201,8 @@ namespace deneme
             foreach (MethodInfo method in MethodInfoList)
             {
                 Query = $"INSERT INTO `methodinfo`(`method_id`,`method_namespace`, `method_class`,`method_name`, `method_parameters`, `method_returntype` ) VALUES ('" + method.Id+ "','" + method.NameSpace + "','" + method.ClassName + "','" + method.MethodName + "','" + method.Parameters + "','" + method.ReturnType + "')";
-                                  
                 cmd.CommandText =Query;
                 cmd.ExecuteNonQuery();
-               
-               
             }
             foreach (MethodBody methodBody in MethodBodyList)
             {
@@ -234,8 +210,6 @@ namespace deneme
                 cmd.CommandText = Query;
                 cmd.ExecuteNonQuery();
                 counter++;
-
-
             }
         }
         public static void findCurrentMethod()
