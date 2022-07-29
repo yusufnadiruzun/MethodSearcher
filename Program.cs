87ddc974e7b413ecfb5e0e8054ae11dc5b9ddc28
Yusuf Nadir Uzun <69757325+yusufnadiruzun@users.noreply.github.com>
2022-07-29 10:54:56 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,25 +16,21 @@ namespace deneme
         public static MySqlCommand cmd = new MySqlCommand();
         public static List<MethodInfo> MethodInfoList = new List<MethodInfo>();
         public static List<MethodBody> MethodBodyList = new List<MethodBody>();
-        public static string path = @"C:\Users\Ruveyda Sultan Uzun\source\repos\MethodSearcher\MethodSearcher\bin\Debug\net6.0\MethodSearcher.dll";
+        public static string path = @"C:\Users\Ruveyda Sultan Uzun\source\repos\denemeMethods\denemeMethods\bin\Debug\net6.0\denemeMethods.dll";
         public static Assembly SampleAssembly = Assembly.LoadFile(path);
-
-
+        Dictionary<string, List<MethodInfo>> searcherResultList = new Dictionary<string, List<MethodInfo>>();
         public static void getMethods()
         {
             int counter = 0;
-            var item = SampleAssembly.GetTypes().Where(x => !(x.FullName.StartsWith("System.")) && !(x.FullName.StartsWith("Microsoft."))).ToList();
-            string ownClassName = item[item.Count - 1].FullName.Substring(0, item[item.Count - 1].FullName.IndexOf("<") - 1);
+            var types = SampleAssembly.GetTypes().Where(x => !(x.FullName.StartsWith("System.")) && !(x.FullName.StartsWith("Microsoft."))).ToList();
             string className;
             string namespaceValue;
-            for (int i = item.Count - 2; i > 0; i--)
+            for (int i = types.Count - 1; i >= 0; i--)
             {
-                if (item[i].Namespace == null || item[i].FullName == ownClassName)
-                {
-                    className = item[i].Name;
-                    namespaceValue= item[i].Namespace;
+                    className = types[i].Name;
+                    namespaceValue = types[i].Namespace;
 
-                    foreach (var method in item[i].GetMethods())
+                    foreach (var method in types[i].GetMethods())
                     {
                         if (!method.Name.Contains("GetHashCode") && !method.Name.Contains("GetType") && !method.Name.Contains("Equals") && !method.Name.Contains("GetType") && !method.Name.Contains("ToString"))
                         {
@@ -50,33 +46,31 @@ namespace deneme
                             MethodInfoList.Add(methodItem);
                             counter++;
                         }
-                    }
+                 
                 }
             }
         }
         public static void getParameters()
         {
             var types = SampleAssembly.GetTypes().Where(x => !(x.FullName.StartsWith("System.")) && !(x.FullName.StartsWith("Microsoft."))).ToList();
-            string ownClassName = types[types.Count - 1].FullName.Substring(0, types[types.Count - 1].FullName.IndexOf("<") - 1);
+           // string ownClassName = types[types.Count - 1].FullName.Substring(0, types[types.Count - 1].FullName.IndexOf("<") - 1);
             string parameters = "";
 
-            for (int i = types.Count - 2; i > 0; i--)
+            for (int i = types.Count - 1; i >= 0; i--)
             {
-                if (types[i].Namespace == null || types[i].FullName == ownClassName)
-                {
                     foreach (var method in types[i].GetMethods())
                     {
                         foreach (var parameteres in method.GetParameters())
                         {
-                            parameters += "," + parameteres.ParameterType.ToString().Substring(parameteres.ParameterType.ToString().IndexOf(".") + 1).ToLower();                           
+                            parameters += "," + parameteres.ParameterType.ToString().Substring(parameteres.ParameterType.ToString().IndexOf(".") + 1).ToLower();
                         }
                         try
                         {
-                            parameters = parameters.Substring(parameters.IndexOf(",")+1);
+                            parameters = parameters.Substring(parameters.IndexOf(",") + 1);
                             char.ToUpper(parameters[0]);
                         }
-                        catch (Exception e){}
-                        
+                        catch (Exception e) { }
+
                         foreach (MethodInfo classitem in MethodInfoList)
                         {
                             if (classitem.MethodName.Equals(method.Name))
@@ -86,14 +80,14 @@ namespace deneme
                         }
                         parameters = "";
                     }
-                }
+             
             }
         }
 
         public static void getMethodBody()
         {
             var assembly = AssemblyDefinition.ReadAssembly(path);
-           
+
             foreach (MethodInfo citem in MethodInfoList)
             {
                 var toInspect = assembly.MainModule
@@ -112,6 +106,7 @@ namespace deneme
                 }
             }
         }
+        
         public static void takeInstructions(Instruction instruction, int citem)
         {
             string[] instructionArray;
@@ -130,17 +125,17 @@ namespace deneme
                     && !instruction.Operand.ToString().Contains("<>c") && !instruction.Operand.ToString().Equals("(") && !instruction.Operand.ToString().Equals(",") && !instruction.Operand.ToString().Equals(")") && !instruction.Operand.ToString().Equals(" ") && !instruction.Operand.ToString().Contains("get_Item") && !instruction.Operand.ToString().Contains("get_Length")
                     && !instruction.Operand.ToString().Contains("Enumerator") && !instruction.Operand.ToString().Contains("ctor") && !instruction.Operand.ToString().Contains("get_Operand") && !instruction.Operand.ToString().Contains("StartsWith") && !instruction.Operand.ToString().Contains("ToUpper") &&
                     !instruction.Operand.ToString().Contains("ToLower") && !instruction.Operand.ToString().Contains("Equals")
-                    && !instruction.Operand.ToString().Contains("Add") && !instruction.Operand.ToString().Contains("ToString") && !instruction.Operand.ToString().Contains("ToList") && !instruction.Operand.ToString().Contains("Write") && !instruction.Operand.ToString().Contains("Dispose") && !instruction.Operand.ToString().Contains("Enumerable") && 
+                    && !instruction.Operand.ToString().Contains("Add") && !instruction.Operand.ToString().Contains("ToString") && !instruction.Operand.ToString().Contains("ToList") && !instruction.Operand.ToString().Contains("Write") && !instruction.Operand.ToString().Contains("Dispose") && !instruction.Operand.ToString().Contains("Enumerable") &&
                     !instruction.Operand.ToString().Contains("IL") && !instruction.Operand.ToString().Contains("Concat")){
-
-
+                    
+                   
                     instructionArray = (instruction.Operand).ToString().Split(":");
                     methodInfo = instructionArray[2].Substring(instructionArray[1].IndexOf(":") + 1);
-                    methodNamespace = instructionArray[0].Substring(instructionArray[0].IndexOf(" ") +1);
+                    methodNamespace = instructionArray[0].Substring(instructionArray[0].IndexOf(" ") + 1);
                     methodTitle = methodNamespace.Split(".");
-                   
-                    if(methodTitle.Length == 3) { methodNamespace = methodTitle[0] + "." + methodTitle[1]; className = methodTitle[2]; } else { methodNamespace = methodTitle[0]; className = methodTitle[1]; }
-                   
+
+                    if (methodTitle.Length == 3) { methodNamespace = methodTitle[0] + "." + methodTitle[1]; className = methodTitle[2]; } else { methodNamespace = methodTitle[0]; className = methodTitle[1]; }
+
                     returnType = instructionArray[0].Substring(0, instructionArray[0].IndexOf(" ") + 1);
                     parameters = methodInfo.Split(".");
 
@@ -163,16 +158,13 @@ namespace deneme
                         MethodName = methodInfo.Substring(0, methodInfo.IndexOf("(")),
                         MethodType = returnType,
                         Parameters = parameter
-
                     };
                     MethodBodyList.Add(ci);
                 }
             }
-            catch (Exception e)
-            {
-
-            }
+            catch (Exception e) { }
         }
+        
         public static void ConnectMysql()
         {
             try
@@ -182,11 +174,11 @@ namespace deneme
                 {
                     Console.WriteLine("SQL Bağlantısı Başarılı Bir Şekilde Gerçekleşti");
                     cmd.Connection = Mysql;
-                    cmd.CommandText = "DELETE FROM methods.methodinfo";
-                    //cmd.CommandText = "";
+                    /*cmd.CommandText = "DELETE FROM methods.methodinfo";
+                    cmd.CommandText = "";
                     cmd.ExecuteNonQuery();
                     cmd.CommandText = "DELETE FROM methods.methodbody";
-                    cmd.ExecuteNonQuery();
+                    cmd.ExecuteNonQuery();*/
                 }
                 else { Console.WriteLine("Bağlantı Yapılamadı...!"); }
             }
@@ -200,8 +192,8 @@ namespace deneme
             int counter = 0;
             foreach (MethodInfo method in MethodInfoList)
             {
-                Query = $"INSERT INTO `methodinfo`(`method_id`,`method_namespace`, `method_class`,`method_name`, `method_parameters`, `method_returntype` ) VALUES ('" + method.Id+ "','" + method.NameSpace + "','" + method.ClassName + "','" + method.MethodName + "','" + method.Parameters + "','" + method.ReturnType + "')";
-                cmd.CommandText =Query;
+                Query = $"INSERT INTO `methodinfo`(`method_id`,`method_namespace`, `method_class`,`method_name`, `method_parameters`, `method_returntype` ) VALUES ('" + method.Id + "','" + method.NameSpace + "','" + method.ClassName + "','" + method.MethodName + "','" + method.Parameters + "','" + method.ReturnType + "')";
+                cmd.CommandText = Query;
                 cmd.ExecuteNonQuery();
             }
             foreach (MethodBody methodBody in MethodBodyList)
@@ -212,44 +204,77 @@ namespace deneme
                 counter++;
             }
         }
-        public static void findCurrentMethod()
+        
+        public static void SearchUserMethod(string Namespace, string className, string methodName, string parameters, string returnType)
         {
-            Console.WriteLine("bitti");
-        }
+            ConnectMysql();
+            List<MethodInfo> methodList = new List<MethodInfo>();
+            string Query = "Select m.method_namespace,m.method_class,m.method_name,m.method_parameters From methods.methodinfo m,  methods.methodbody b where m.method_id = b.mainmethodid and b.methodname = '"+ methodName+"' and b.classname = '" + className + "' and b.namespace = '" + Namespace + "' and b.parameters = '" + parameters + "' ";
 
-        static void Main(string[] args)
+            Console.WriteLine("searcher içi");
+
+            cmd.CommandText = Query;
+            Mysql reader = cmd.ExecuteScalar();
+
+            while (reader.Read())
+            {
+                MethodInfo method = new MethodInfo
+                {
+                    MethodName = reader["method_name"].ToString(),
+                    NameSpace = reader["method_namespace"].ToString(),
+                    ClassName = reader["method_class"].ToString(),
+                    Parameters = reader["method_parameters"].ToString()
+                };
+                Console.WriteLine("searcher içireader");
+                methodList.Add(method);
+            }
+            foreach (MethodInfo m in methodList)
+            {
+                Console.WriteLine("namespace :" + m.NameSpace + "classname :"+ m.ClassName +"name : " + m.MethodName + " parameters : "+ m.Parameters);
+            }
+        }
+        public static void getAllMethods()
         {
-            Console.WriteLine("Method İsimleri");
+        
             getMethods();
-            Console.WriteLine("Method namespace and class");
-          //  getProperties();
-            Console.WriteLine("Method parameters");
-             getParameters();
-            Console.WriteLine("Method body");
-             getMethodBody();
+            getParameters();
+            getMethodBody();
             WriteDB();
-            findCurrentMethod();
-            //ConnectMysql();
+        }
+       
+        static void Main(string[] args)
+            {
+              // method Info : 
+              string nameSpace = "class3Namespace";
+              string className = "Class3";
+              string methodName = "class3Function2";
+              string returnType = "System.Void";
+              string parameters = "string,ınt32";
+
+              getAllMethods();
+              SearchUserMethod(nameSpace, className, methodName, parameters,returnType);
+           
+            }
         }
     }
-}
+
 public class MethodInfo
-{
-    public MethodInfo() { }
-    public int Id { get; set; }
-    public int SubId { get; set; }
-    public string? NameSpace { get; set; }
-    public string? ClassName { get; set; }
-    public string? MethodName { get; set; }
-    public string? Parameters { get; set; }
-    public string? ReturnType { get; set; }
-}
+    {
+        public MethodInfo() { }
+        public int Id { get; set; }
+        public int SubId { get; set; }
+        public string? NameSpace { get; set; }
+        public string? ClassName { get; set; }
+        public string? MethodName { get; set; }
+        public string? Parameters { get; set; }
+        public string? ReturnType { get; set; }
+    }
 public class MethodBody
-{
-    public string? NameSpace { get; set; }
-    public string? Classname { get; set; }
-    public int? MainMethodID { get; set; }
-    public string? MethodName { get; set; }
-    public string? MethodType { get; set; }
-    public string? Parameters { get; set; }
-}
+    {
+        public string? NameSpace { get; set; }
+        public string? Classname { get; set; }
+        public int? MainMethodID { get; set; }
+        public string? MethodName { get; set; }
+        public string? MethodType { get; set; }
+        public string? Parameters { get; set; }
+    }
